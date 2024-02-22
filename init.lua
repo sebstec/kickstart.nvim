057a27e3599a39a180782d4de7455ecccc4709ad
sebste <sebstec@vikingtec.ninja>
2024-02-22 12:23:03 +0100

    fixed illuminate

--- init.lua
+++ init.lua
@@ -68,6 +68,8 @@ vim.opt.rtp:prepend(lazypath)
 --    as they will be available in your neovim runtime.
 require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
+  -- (S) illuminate
+  'RRethy/vim-illuminate',
 
   -- Git related plugins
   'tpope/vim-fugitive',
