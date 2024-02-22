da2479c42ef56bc68de005b270757f49f1e17533
sebste <sebstec@vikingtec.ninja>
2024-02-22 12:44:20 +0100

    added templ filetype

--- init.lua
+++ init.lua
@@ -743,5 +743,8 @@ cmp.setup {
   },
 }
 
+-- (S) FILETYPES 
+vim.filetype.add({ extension = { templ = "templ" } })
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
