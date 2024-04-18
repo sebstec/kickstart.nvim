1eac9a73065e658a74e86728259d6dd9d6eda1ac
sebste <sebstec@vikingtec.ninja>
2024-04-18 19:44:33 +0200

    vue formatting

--- lua/custom/plugins/formatter.lua
+++ lua/custom/plugins/formatter.lua
@@ -51,6 +51,15 @@ return {
         javascriptreact = {
           require("formatter.filetypes.javascriptreact").prettierd
         },
+        vue = {
+          function()
+            return {
+              exe = "prettierd",
+              args = { util.escape_path(util.get_current_buffer_file_path()) },
+              stdin = true,
+            }
+          end
+        },
         go = {
           require("formatter.filetypes.go").gofmt
         },
