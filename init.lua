a63def5d013bba869c9c4925ccb6a789474c38b8
sebste <sebstec@vikingtec.ninja>
2025-09-02 12:34:22 +0200

    feat: add shfmt

--- init.lua
+++ init.lua
@@ -818,14 +818,20 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { "ruff","isort", "black", stop_after_first = true },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { "prettierd", "prettier", stop_after_first = true },
         javascriptreact = { "prettierd", "prettier", stop_after_first = true },
         astro = { "prettierd", "prettier", stop_after_first = true },
         css = { "prettierd", "prettier", stop_after_first = true },
+        sh = {"shfmt", stop_after_first = true }
       },
+      formatters = {
+        shfmt = {
+          append_args = {"-i", "2"}
+        }
+      }
     },
   },
 
